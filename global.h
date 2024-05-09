a47878ef06885f44862481a378796081ca06824c
zhoumei <183242238@qq.com>
2024-05-09 15:09:54 +0800

    注册获取验证码，正常注册

--- global.h
+++ global.h
@@ -3,11 +3,16 @@
 
 #include <QString>
 #define HTTP_SERVER_DOMAIN_ADDRESS "http://120.26.132.153:8080"
-#define HTTP_YSY_LOGIN              "/api/login"           //登录
+#define HTTP_YSY_PASSWORD_LOGIN     "/api/login"           //密码登录
 #define HTTP_YSY_REGISTER           "/api/login/register"   //注册
+#define HTTP_YSY_LOGOUT             "/api/login/logout"     //注销
 #define HTTP_YSY_SMS_LOGIN          "/api/login/sms"        //短信验证码登录
 #define HTTP_YSY_GET_SMS_CODE       "/api/sms"             //获取验证码
 
+#define HTTP_QUERY_ALL_GROUP        "/api/group/list"       //查询全部分组
+#define HTTP_CREATE_GROUP           "/api/group/create"     //创建分组
+#define HTTP_UPDATE_GROUP           "/api/group/update"     //修改分组
+#define HTTP_DELETE_GROUP           "/api/group/delete/{id}" //删除分组
 
 #define HTTP_SUCCESS_CODE           200
 
@@ -27,4 +32,19 @@ enum ENUM_LOGIN_STATUS
     TYPE_WINDOWS_SHOW = 1,
     TYPE_WINDOWS_CLOSE = 2
 };
+
+typedef struct USER_LOGIN_INFO
+{
+    int     id;
+    QString strMaxExpirationDate;
+    QString strToken;
+    QString strAccount;
+    QString strMobile;
+    QString strName;
+    QString strPhotoUrl;
+    USER_LOGIN_INFO()
+    {
+        memset(this, 0, sizeof(USER_LOGIN_INFO));
+    }
+}S_USER_LOGIN_INFO,*P_USER_LOGIN_INFO;
 #endif // GLOBAL_H
--- registerpage.cpp
+++ registerpage.cpp
@@ -115,3 +115,68 @@ void RegisterPage::on_btnReturn_clicked()
     emit showPageType(TYPE_PASSWORDLOGIN_PAGE);
 }
 
+
+void RegisterPage::on_pushButton_clicked()
+{
+    //获取验证码
+    qDebug() << "获取验证码";
+    QString strPhone = ui->lineEditPhone->text();
+    if(strPhone.isEmpty())
+    {
+        QMessageBox::warning(this,"提示","输入手机号码不能为空");
+        return;
+    }
+
+
+    QString strUrl = HTTP_SERVER_DOMAIN_ADDRESS;
+    strUrl += HTTP_YSY_GET_SMS_CODE;
+    strUrl += "/?mobiles=" + strPhone;
+    //创建网络访问管理器
+    QNetworkAccessManager* manager = new QNetworkAccessManager(this);
+
+    QUrl url(strUrl);
+    /*QUrlQuery query;
+    query.addQueryItem("mobiles", strPhone);
+    url.setQuery(query.query(QUrl::FullyEncoded));*/
+    //创建请求对象
+    QNetworkRequest request;
+    request.setRawHeader("Authorization", strPhone.toUtf8());
+    request.setUrl(url);
+
+    QNetworkReply* reply = manager->get(request);
+    QNetworkReply::NetworkError error = reply->error();
+    qDebug() << "reply error=" << error;
+    //连接请求完成的信号
+    connect(reply, &QNetworkReply::finished, this, [=] {
+        if (reply->error())
+        {
+            qDebug() << "获取验证码报错:" << reply->error();
+        }
+        else
+        {
+            //读取响应数据
+            QByteArray response = reply->readAll();
+            qDebug() << response;
+
+            QJsonParseError parseError;
+            QJsonDocument doc = QJsonDocument::fromJson(response, &parseError);
+            if (parseError.error != QJsonParseError::NoError)
+            {
+                qDebug() << response;
+                qWarning() << "Json parse error:" << parseError.errorString();
+            }
+            else
+            {
+                if (doc.isObject())
+                {
+                    QJsonObject obj = doc.object();
+                    int iCode = obj["code"].toInt();
+                    QString strMessage = obj["message"].toString();
+                    qDebug() << "Code=" << iCode << "message=" << strMessage;
+                }
+            }
+        }
+        reply->deleteLater();
+    });
+}
+
