417ebb93395cf92a8e3de488fd9c91c653e0d4c7
zhoumei <183242238@qq.com>
2024-05-30 15:41:11 +0800

    添加点击事件判断

--- videoviewwidget.cpp
+++ videoviewwidget.cpp
@@ -29,15 +29,76 @@ void VideoViewWidget::mousePressEvent(QMouseEvent *event)
     qDebug() << "Global point.x=" << point.x() << "point.y=" << point.y();
     qDebug() << "Local point.x=" << localPoint.x() << "point.y=" << localPoint.y();
 
-    //int mouseX = event->
+	int mouseX = localPoint.x()-this->rect().left();
+	int mouseY = localPoint.y()-this->rect().top();
+
+	int viewWidth = this->rect().right() -this->rect().left();
+	int viewHeight = this->rect().bottom() - this->rect().top();
+	qDebug() << "viewWidth=" << viewWidth << "viewHeight=" << viewHeight;
+	int videoWidth = getSrcWidth();
+	int videoHeight = getSrcHeight();
+	qDebug() << "videoWidth=" << videoWidth << "videoHeight=" << videoHeight;
+
+	float percentX = mouseX / (float)(viewWidth * 1.0f);
+	float percentY = mouseY / (float)(viewHeight * 1.0f);
+
+	int x[1] = { -1 };
+	int y[1] = { -1 };
+	float force[1] = { 0 };
+
+	x[0] = (int)(videoWidth * percentX);
+	y[0] = (int)(videoHeight * percentY);
+
+	OnTouchEvent(SW_ACTION_DOWN, 1, x, y, force);
+	clicktype = SW_ACTION_DOWN;
+	
+	QWidget::mousePressEvent(event);
 }
 void VideoViewWidget::mouseReleaseEvent(QMouseEvent *event)
 {
-
+	int x[1] = { -1 };
+	int y[1] = { -1 };
+	float force[1] = { 0 };
+	x[0] = -1;
+	y[0] = -1;
+	OnTouchEvent(SW_ACTION_UP, 1, x, y, force);
+	clicktype = SW_ACTION_UP;
+	QWidget::mousePressEvent(event);
 }
-void VideoViewWidget::mouseMoveEvent(QMouseEvent *event)
+void VideoViewWidget::mouseMoveEvent(QMouseEvent *event) 
 {
+	if (clicktype == SW_ACTION_DOWN)
+	{
+		QPointF mousePoint = event->localPos();
+		if (mousePoint.x() > this->rect().right()|| mousePoint.y() > this->rect().bottom()) 
+		{
+			event->ignore();
+			return;
+		}
+		int mouseX = mousePoint.x() - this->rect().left();
+		int mouseY = mousePoint.y() - this->rect().top();
 
+		int viewWidth = this->rect().right() - this->rect().left();
+		int viewHeight = this->rect().bottom() - this->rect().top();
+
+		int videoWidth = getSrcWidth();
+		int videoHeight = getSrcHeight();
+
+		float percentX = mouseX / (float)(viewWidth * 1.0f);
+		float percentY = mouseY / (float)(viewHeight * 1.0f);
+
+		int x[1] = { -1 };
+		int y[1] = { -1 };
+		float force[1] = { 0 };
+
+		x[0] = (int)(videoWidth * percentX);
+		y[0] = (int)(videoHeight * percentY);
+
+		OnTouchEvent(SW_ACTION_MOVE, 1, x, y, force);
+		event->accept();
+		return;
+	}
+	QWidget::mouseMoveEvent(event);
 }
 void VideoViewWidget::paintEvent(QPaintEvent *event)
 {
