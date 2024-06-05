64b42b5fe94ad095484eb4f8f9cfbf6214f1987a
zhoumei <183242238@qq.com>
2024-06-05 17:48:01 +0800

    关于我们页面ok

--- systemsettingwidget.cpp
+++ systemsettingwidget.cpp
@@ -1,6 +1,7 @@
 #include "systemsettingwidget.h"
 #include "ui_systemsettingwidget.h"
 #include "messagetips.h"
+#include <QDesktopServices>
 
 SystemSettingWidget::SystemSettingWidget(QWidget *parent)
     : QWidget(parent)
@@ -21,6 +22,8 @@ void SystemSettingWidget::on_toolBtnGeneralSetting_clicked()
     ui->toolBtnGeneralSetting->setStyleSheet("QToolButton {border:none;color:#FF505465;background-color:#FFFFFFFF;border-radius:0px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #FF505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnUISetting->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnAbout->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
+
+    ui->frame_7->setVisible(true);
     ui->stackedWidget->setCurrentWidget(ui->pageGeneralSetting);
 }
 
@@ -30,6 +33,8 @@ void SystemSettingWidget::on_toolBtnUISetting_clicked()
     ui->toolBtnUISetting->setStyleSheet("QToolButton {border:none;color:#FF505465;background-color:#FFFFFFFF;border-radius:0px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #FF505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnGeneralSetting->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnAbout->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
+
+    ui->frame_7->setVisible(true);
     ui->stackedWidget->setCurrentWidget(ui->pageUISetting);
 }
 
@@ -39,6 +44,8 @@ void SystemSettingWidget::on_toolBtnAbout_clicked()
     ui->toolBtnAbout->setStyleSheet("QToolButton {border:none;color:#FF505465;background-color:#FFFFFFFF;border-radius:0px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #FF505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnGeneralSetting->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
     ui->toolBtnUISetting->setStyleSheet("QToolButton {border:none;color:#505465;border-radius:1px;padding-left:16px;}QToolButton:hover {background-color: #FFFFFFFF;color: #505465;border-radius:0px;padding-left:16px;}");
+
+    ui->frame_7->setVisible(false);
     ui->stackedWidget->setCurrentWidget(ui->pageAbout);
 }
 
@@ -61,5 +68,40 @@ void SystemSettingWidget::on_btnSave_clicked()
     //保存
     MessageTips* tips = new MessageTips("数据已保存",this);
     tips->show();
+}
+
+
+void SystemSettingWidget::on_pushButton_3_clicked()
+{
+    QString strUrl = "https://www.ysyos.com/";
+    QDesktopServices::openUrl(QUrl(strUrl));
+}
+
+
+void SystemSettingWidget::on_pushButton_4_clicked()
+{
+    QString strUrl = "https://www.ysyos.com/deal/Disclaimer.html";
+    QDesktopServices::openUrl(QUrl(strUrl));
+}
+
+
+void SystemSettingWidget::on_pushButton_5_clicked()
+{
+    QString strUrl = "https://www.ysyos.com/deal/UserPrivacy.html";
+    QDesktopServices::openUrl(QUrl(strUrl));
+}
+
+
+void SystemSettingWidget::on_pushButton_6_clicked()
+{
+    QString strUrl = "https://www.ysyos.com/deal/PrivacyPolicy.html";
+    QDesktopServices::openUrl(QUrl(strUrl));
+}
+
+
+void SystemSettingWidget::on_pushButton_7_clicked()
+{
+    QString strUrl = "https://www.ysyos.com/deal/VipPrivacy.html";
+    QDesktopServices::openUrl(QUrl(strUrl));
 }
 
