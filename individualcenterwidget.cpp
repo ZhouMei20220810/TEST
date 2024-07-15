0da857f0d184813f6d89ea3504ec58fcdabdfbf3
zhoumei <183242238@qq.com>
2024-07-15 11:39:40 +0800

    个人中心添加阴影边框

--- individualcenterwidget.cpp
+++ individualcenterwidget.cpp
@@ -10,6 +10,7 @@
 #include "buyhistorywidget.h"
 #include "authorizationmanagedialog.h"
 #include "activecodehistorydialog.h"
+#include <QGraphicsDropShadowEffect>
 
 IndividualCenterWidget::IndividualCenterWidget(QWidget *parent)
     : QDialog(parent)
@@ -18,6 +19,13 @@ IndividualCenterWidget::IndividualCenterWidget(QWidget *parent)
     ui->setupUi(this);
     setAttribute(Qt::WA_DeleteOnClose, true);
     setWindowFlags(Qt::FramelessWindowHint);
+
+    QGraphicsDropShadowEffect* shadow = new QGraphicsDropShadowEffect();
+    shadow->setBlurRadius(5);//阴影模糊半径
+    shadow->setXOffset(0);//水平偏移
+    shadow->setYOffset(0); //垂直偏移
+    shadow->setColor(Qt::gray);//阴影颜色
+    this->setGraphicsEffect(shadow);
 
     //QFontMetrics fontWidth(ui->labelAccount->font());
     //QString strElideNote = fontWidth.elidedText(GlobalData::strAccount, Qt::ElideRight, 38);
