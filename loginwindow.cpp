e23fdd1a61afbc6d86e73f3c5a620de905b59d9e
zhoumei <183242238@qq.com>
2024-07-16 13:46:40 +0800

    登录界面增加阴影边框

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -16,12 +16,13 @@ LoginWindow::LoginWindow(QWidget *parent)
     ui->setupUi(this);
     setWindowFlags(Qt::FramelessWindowHint);
     setAttribute(Qt::WA_Hover, true);
+    setAttribute(Qt::WA_TranslucentBackground, true);
     QGraphicsDropShadowEffect* shadow = new QGraphicsDropShadowEffect();
     shadow->setBlurRadius(5);//阴影模糊半径
     shadow->setXOffset(0);//水平偏移
     shadow->setYOffset(0); //垂直偏移
     shadow->setColor(Qt::gray);//阴影颜色
-    ui->centralwidget->setGraphicsEffect(shadow);
+    this->setGraphicsEffect(shadow);
 
     m_passwordLoginPage = new PasswordLoginPage(this);
     m_smsLoginPage = new SMSLoginPage(this);
