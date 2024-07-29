eee4ccc6a17ad9ca6dfda1023e6062f6ae666411
zhoumei <183242238@qq.com>
2024-07-29 10:11:18 +0800

    线上版本v1.0.0

--- global.h
+++ global.h
@@ -4,7 +4,7 @@
 #include <QToolButton>
 #include <QString>
 #include <QSystemTrayIcon>
-#define CURRENT_APP_VERSION         "1.0.0"
+#define CURRENT_APP_VERSION         "1.0.0" //线上版本
 #define CURRENT_VERSION_PRODUCTCODE "{5E87264F-FCFF-4987-9B21-B7E2C678F7C3}"        //当前版本的ProductCode每次软件升级需修改
 
 #define HTTP_SERVER_DOMAIN_ADDRESS  "http://120.26.132.153:8080"  //"http://192.168.1.6:8080"  //        //
