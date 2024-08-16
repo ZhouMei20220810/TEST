b743b9ef419ae0fd3314f01c4ef4b9144b156000
zhoumei <183242238@qq.com>
2024-08-16 11:35:28 +0800

    复制状态窗口，支持move和边框，状态文字自适应，当前显示一条记录ok

--- recentcopystatusdialog.cpp
+++ recentcopystatusdialog.cpp
@@ -2,8 +2,82 @@
 #include "ui_recentcopystatusdialog.h"
 #include <QGraphicsDropShadowEffect>
 
+CopyStatusItem::CopyStatusItem(S_PHONE_INFO info, EN_COPY_STATUS enType, QWidget* parent)
+    : QWidget(parent)
+{
+    resize(COPY_STATUS_ITEM_WIDTH, COPY_STATUS_ITEM_HEIGHT);
+    QVBoxLayout* vLayout = new QVBoxLayout(this);
+    vLayout->setContentsMargins(0, 0, 0, 0);
+    QHBoxLayout* hLayout = new QHBoxLayout(this);
+    hLayout->setContentsMargins(0, 0, 0, 0);
+
+    QString strStyleSheet = "background:transparent;color:#596380;font-size:12px;font-weight:bold;";
+    m_LabelInstance = new QLabel(this);
+    //这里设置为非互斥，使用QButtonGroup来控制
+    //connect(m_radioBtnContent, &QRadioButton::clicked, this, &RecentListItem::selectItemSignals);
+    m_LabelInstance->setStyleSheet(strStyleSheet);
+    //radioBtnContent->setFixedSize(QSize(RECENT_LIST_ITEM_WIDTH - 50, RECENT_LIST_ITEM_HEIGHT));
+    QFontMetrics fontWidth(m_LabelInstance->font());
+    QString strElideNote = fontWidth.elidedText(info.strName, Qt::ElideRight, COPY_STATUS_ITEM_WIDTH - 50);
+    m_LabelInstance->setText(strElideNote);
+    m_LabelInstance->setToolTip(info.strName);
+
+
+    hLayout->addWidget(m_LabelInstance);
+    hLayout->addStretch();
+
+    m_LabelStatus = new QLabel(this);
+    //成功
+    switch (enType)
+    {
+    case EN_COPY_STATUS_SUCCESS:    //成功
+    {
+        strStyleSheet = "background:transparent;color:#3071F2;font-size:12px;font-weight:bold;";
+        m_LabelStatus->setText(QString("粘贴成功").toUtf8());
+    }
+    break;
+    case EN_COPY_STATUS_FAILD:       //错误
+    {
+        strStyleSheet = "background:transparent;color:#FF4D4D;font-size:12px;font-weight:bold;";
+        m_LabelStatus->setText(QString("失败").toUtf8());
+    }
+    break;
+    case EN_COPY_STATUS_NO_CONTENT:   //无内容
+    {
+        strStyleSheet = "background:transparent;color:#596380;font-size:12px;font-weight:bold;";
+        m_LabelStatus->setText(QString("无内容").toUtf8());
+    }
+    break;
+    default:
+        break;
+    }
+
+    m_LabelStatus->setStyleSheet(strStyleSheet);
+    hLayout->addWidget(m_LabelStatus);
+
+    //添加到垂直布局
+    vLayout->addLayout(hLayout);
+    this->setLayout(vLayout);
+}
+
+CopyStatusItem::~CopyStatusItem()
+{
+    qDebug() << "delete ~CopyStatusItem";
+    delete this;
+}
+
+void CopyStatusItem::setCopyStatus(EN_COPY_STATUS enType)
+{
+    m_enStatus = enType;
+}
+
+EN_COPY_STATUS CopyStatusItem::getCopyStatus()
+{
+    return m_enStatus;
+}
+
 RecentCopyStatusDialog::RecentCopyStatusDialog(QWidget *parent)
-    : QDialog(parent)
+    : QMoveDialog(parent)
     , ui(new Ui::RecentCopyStatusDialog)
 {
     ui->setupUi(this);
@@ -48,11 +122,70 @@ void RecentCopyStatusDialog::on_btnOK_clicked()
     this->close();
 }
 
-void RecentCopyStatusDialog::do_CopyStatusSignals(S_PHONE_INFO info, int iRet)
+void RecentCopyStatusDialog::do_CopyStatusSignals(S_PHONE_INFO info, int iRet,QString strContent)
 {
     qDebug() << "新增一条记录" << info.strInstanceNo << "iRet=" << iRet;
     QListWidgetItem* item = new QListWidgetItem(ui->listWidget);
-    CopyStatusItem* widget = new CopyStatusItem(info, iRet, this);
+    
+    EN_COPY_STATUS enType;
+    if (!strContent.isEmpty())
+    {
+        if (iRet == 0)
+            enType = EN_COPY_STATUS_SUCCESS;
+        else
+            enType = EN_COPY_STATUS_FAILD;
+    }
+    else
+    {
+        enType = EN_COPY_STATUS_NO_CONTENT;
+    }
+    item->setData(Qt::UserRole, enType);
+    CopyStatusItem* widget = new CopyStatusItem(info, enType, this);
     item->setSizeHint(QSize(COPY_STATUS_ITEM_WIDTH, COPY_STATUS_ITEM_HEIGHT));
     ui->listWidget->setItemWidget(item, widget);
+
+    //显示下方统计成功、失败、无内容
+    calcCopyStatusResult();
+}
+
+//统计拷贝状态结果
+void RecentCopyStatusDialog::calcCopyStatusResult()
+{
+    int iListCount = ui->listWidget->count();
+    int iSuccess = 0;
+    int iFailed = 0;
+    int iNoContent = 0;
+    QListWidgetItem* item = NULL;
+    EN_COPY_STATUS enType;
+    for (int row = 0; row < iListCount; row++)
+    {
+        item = ui->listWidget->item(row);
+        if (item != NULL)
+        {
+            enType = (EN_COPY_STATUS)item->data(Qt::UserRole).toInt();
+            switch (enType)
+            {
+            case EN_COPY_STATUS_SUCCESS:    //成功
+            {
+                iSuccess++;
+            }
+            break;
+            case EN_COPY_STATUS_FAILD:       //错误
+            {
+                iFailed++;
+            }
+            break;
+            case EN_COPY_STATUS_NO_CONTENT:   //无内容
+            {
+                iNoContent++;
+            }
+            break;
+            default:
+                break;
+            }
+        }
+    }
+    ui->labelSuccess->setText(QString("成功：%1台").arg(iSuccess));
+    ui->labelFailed->setText(QString("失败：%1台").arg(iFailed));
+    ui->labelNoContent->setText(QString("无内容：%1台").arg(iNoContent));
 }
--- recentcopystatusdialog.h
+++ recentcopystatusdialog.h
@@ -1,7 +1,7 @@
 #ifndef RECENTCOPYSTATUSDIALOG_H
 #define RECENTCOPYSTATUSDIALOG_H
 
-#include <QDialog>
+#include "qmovedialog.h"
 #include <QLabel>
 #include "global.h"
 #include <QVBoxLayout>
@@ -14,60 +14,29 @@ class RecentCopyStatusDialog;
 #define COPY_STATUS_ITEM_WIDTH          200
 #define COPY_STATUS_ITEM_HEIGHT         30
 
+enum EN_COPY_STATUS
+{
+    EN_COPY_STATUS_SUCCESS = 0,     //成功
+    EN_COPY_STATUS_FAILD = 1,       //错误
+    EN_COPY_STATUS_NO_CONTENT = 2   //无内容
+};
+
 class CopyStatusItem : public QWidget
 {
     Q_OBJECT
 public:
-    CopyStatusItem(S_PHONE_INFO info, int iRet, QWidget* parent = nullptr)
-        : QWidget(parent)
-    {
-        resize(COPY_STATUS_ITEM_WIDTH, COPY_STATUS_ITEM_HEIGHT);
-        QVBoxLayout* vLayout = new QVBoxLayout(this);
-        vLayout->setContentsMargins(0, 0, 0, 0);
-        QHBoxLayout* hLayout = new QHBoxLayout(this);
-        hLayout->setContentsMargins(0, 0, 0, 0);
+    CopyStatusItem(S_PHONE_INFO info, EN_COPY_STATUS enType, QWidget* parent = nullptr);
+    ~CopyStatusItem();
 
-        QString strStyleSheet = "color:#596380;font-size:12px;font-weight:bold;";
-        m_LabelInstance = new QLabel(this);
-        //这里设置为非互斥，使用QButtonGroup来控制
-        //connect(m_radioBtnContent, &QRadioButton::clicked, this, &RecentListItem::selectItemSignals);
-        m_LabelInstance->setStyleSheet(strStyleSheet);
-        //radioBtnContent->setFixedSize(QSize(RECENT_LIST_ITEM_WIDTH - 50, RECENT_LIST_ITEM_HEIGHT));
-        QFontMetrics fontWidth(m_LabelInstance->font());
-        QString strElideNote = fontWidth.elidedText(info.strName, Qt::ElideRight, COPY_STATUS_ITEM_WIDTH - 50);
-        m_LabelInstance->setText(strElideNote);
-        m_LabelInstance->setToolTip(info.strName);
-
-
-        hLayout->addWidget(m_LabelInstance);
-        hLayout->addStretch();
-
-        m_LabelStatus = new QLabel(this);
-        //成功
-        if (iRet == 0)
-        {
-            strStyleSheet = "color:#3071F2;font-size:12px;font-weight:bold;";
-            m_LabelStatus->setText("粘贴成功");
-        }
-        else
-        {
-            strStyleSheet = "color:#FF4D4D;font-size:12px;font-weight:bold;";
-            //m_LabelStatus->setText("失败");
-        }
-        m_LabelStatus->setStyleSheet(strStyleSheet);
-        hLayout->addWidget(m_LabelStatus);
-
-        //添加到垂直布局
-        vLayout->addLayout(hLayout);
-        this->setLayout(vLayout);
-    };
-
+    void setCopyStatus(EN_COPY_STATUS enType);
+    EN_COPY_STATUS getCopyStatus();
 private:
     QLabel* m_LabelInstance;
     QLabel*  m_LabelStatus;
+    EN_COPY_STATUS m_enStatus;
 };
 
-class RecentCopyStatusDialog : public QDialog
+class RecentCopyStatusDialog : public QMoveDialog
 {
     Q_OBJECT
 
@@ -75,11 +44,14 @@ public:
     explicit RecentCopyStatusDialog(QWidget *parent = nullptr);
     ~RecentCopyStatusDialog();
 public slots:
-    void do_CopyStatusSignals(S_PHONE_INFO info, int iRet);
+    void do_CopyStatusSignals(S_PHONE_INFO info, int iRet, QString strContent);
 private slots:
     void on_btnClose_clicked();
 
     void on_btnOK_clicked();
+private:
+    //统计拷贝状态结果
+    void calcCopyStatusResult();
 
 private:
     Ui::RecentCopyStatusDialog *ui;
